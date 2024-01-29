b30fe66b7523ba4d908c905afcd533dc3ca666f4
Volentry <ayushmhetre1234@gmail.com>
2024-01-30 00:02:52 +0530

    made some changes to marketplace

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -3,20 +3,20 @@ import React from "react";
 const Marketplace = ()=>{
    
     return(
-<body className=" ">
+<main>
 
-<div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-72  drop-shadow-md">
+<div className=" mt-20 bg-gray-900 shadow-white h-72   shadow-sm ">
       <h1 className="text-center pt-24 text-white font-bold text-7xl drop-shadow-xl shadow-white">NFT. MARKETPLACE</h1>
 </div>
 
 
-<main className="place-items-center  grid  grid-cols-3  space-y-5   pt-10  pb-10 place-content-evenly   " >
+<main className="place-items-center  grid  grid-cols-4   mt-24  place-content-evenly  pb-10 place-content-evenly   " >
 
   <div className="size-fit ">
-  <div className="border-2  rounded-lg bg-gray-200 space-y-2 drop-shadow-xl shadow-black bg-opacity-25 "> 
+  <div className="  rounded-lg bg-black bg-opacity-15  space-y-2 shadow-sm shadow-black bg-opacity-25 mb-8 "> 
       <img className="size-72" src="https://tse4.mm.bing.net/th?id=OIP.0BLUPEUYgRbRu1-ZmrFe6wHaHa&pid=Api&P=0&h=180"></img>  
-      <div className=""><h1 className="font-bold text-justify size-auto  ">FIFA WORLD CUP SEASON PASS</h1>
-     <div className="pb-3 mt-2 flex  space-x-3 place-items-center  "><p className="text-black font-bold font-mono">Price-$89.00</p>
+      <div className=""><h1 className="font-bold text-white size-auto  ">FIFA WORLD CUP SEASON PASS</h1>
+     <div className="pb-3 mt-2 flex  space-x-3 place-items-center  "><p className="text-white font-bold font-mono">Price-$89.00</p>
      <button className=" font-semibold   bg-gradient-to-r from-cyan-500 to-blue-500 text-white p-3 rounded-full hover:p-4 transition-all">Buy NFT</button></div>
     
     
@@ -25,10 +25,10 @@ const Marketplace = ()=>{
 </div>
 
   <div className="size-fit">
-  <div className="box-border bg-gray-200 border-2 box-content  rounded-lg space-y-2 drop-shadow-xl shadow-black "> 
+  <div className="rounded-lg bg-black bg-opacity-15  space-y-2 shadow-sm shadow-black bg-opacity-25 mb-8 "> 
       <img className="size-72" src="https://preview.redd.it/india-vs-pakistan-in-world-cup-2023-will-be-played-on-15th-v0-yxdnss9eicza1.jpg?auto=webp&s=c7389aa5ea0e463d2f839fae7db2d3f41f9fa74f"></img>  
-      <div className=""><h1 className="font-bold text-justify size-auto ">INDIA VS PAKISTAN  MATCH TICKET</h1>
-     <div className="pb-3 mt-2 flex  space-x-3 place-items-center  "><p className="text-black font-bold  font-mono">Price-$69.00</p>
+      <div className=""><h1 className="font-bold text-white size-auto ">INDIA VS PAKISTAN  MATCH TICKET</h1>
+     <div className="pb-3 mt-2 flex  space-x-3 place-items-center  "><p className="text-white font-bold  font-mono">Price-$69.00</p>
      <button className=" font-semibold   bg-gradient-to-r from-cyan-500 to-blue-500 text-white p-3 rounded-full hover:p-4 transition-all">Buy NFT</button></div>
     
     
@@ -38,7 +38,7 @@ const Marketplace = ()=>{
 
 
      <div className="size-fit  " >
-  <div className="box-border bg-black bg-opacity-15  border-2 box-content space-y-2 rounded-lg drop-shadow-xl shadow-black backdrop-blur-3xl   "> 
+  <div className="rounded-lg bg-black bg-opacity-15  space-y-2 shadow-sm shadow-black bg-opacity-25 mb-8  "> 
       <img className="size-72" src="https://www.patriot-place.com/wp-content/uploads/2023/01/Ed-Sheeran-2023-square-2.jpg"></img>  
       <div className=""><h1 className="font-bold text-justify  size-auto text-white">Ed Sheeran Mathematics Tour </h1>
      <div className="pb-3 mt-2 flex  space-x-3 place-items-center  "><p className="text-white font-bold font-mono">Price-$130.00</p>
@@ -52,10 +52,10 @@ const Marketplace = ()=>{
 
 
  <div className="size-fit ">
-  <div className="border-2  rounded-lg bg-gray-200 space-y-2 drop-shadow-xl shadow-black "> 
+  <div className="rounded-lg bg-black bg-opacity-15  space-y-2 shadow-sm shadow-black bg-opacity-25 mb-8 "> 
       <img className="size-72" src="https://tse4.mm.bing.net/th?id=OIP.0BLUPEUYgRbRu1-ZmrFe6wHaHa&pid=Api&P=0&h=180"></img>  
-      <div className=""><h1 className="font-bold text-justify size-auto ">FIFA WORLD CUP SEASON PASS</h1>
-     <div className="pb-3 mt-2 flex  space-x-3 place-items-center  "><p className="text-black font-bold font-mono">Price-$89.00</p>
+      <div className=""><h1 className="font-bold text-white size-auto ">FIFA WORLD CUP SEASON PASS</h1>
+     <div className="pb-3 mt-2 flex  space-x-3 place-items-center  "><p className="text-white font-bold font-mono">Price-$89.00</p>
      <button className=" font-semibold   bg-gradient-to-r from-cyan-500 to-blue-500 text-white p-3 rounded-full hover:p-4 transition-all">Buy NFT</button></div>
     
     
@@ -64,10 +64,10 @@ const Marketplace = ()=>{
 </div>
 
   <div className="size-fit">
-  <div className="box-border bg-gray-200 border-2 box-content  rounded-lg space-y-2 drop-shadow-xl shadow-black "> 
+  <div className="rounded-lg bg-black bg-opacity-15  space-y-2 shadow-sm shadow-black bg-opacity-25 mb-8 "> 
       <img className="size-72" src="https://preview.redd.it/india-vs-pakistan-in-world-cup-2023-will-be-played-on-15th-v0-yxdnss9eicza1.jpg?auto=webp&s=c7389aa5ea0e463d2f839fae7db2d3f41f9fa74f"></img>  
-      <div className=""><h1 className="font-bold text-justify size-auto ">INDIA VS PAKISTAN  MATCH TICKET</h1>
-     <div className="pb-3 mt-2 flex  space-x-3 place-items-center  "><p className="text-black font-bold  font-mono">Price-$69.00</p>
+      <div className=""><h1 className="font-bold text-white size-auto ">INDIA VS PAKISTAN  MATCH TICKET</h1>
+     <div className="pb-3 mt-2 flex  space-x-3 place-items-center  "><p className="text-white font-bold  font-mono">Price-$69.00</p>
      <button className=" font-semibold   bg-gradient-to-r from-cyan-500 to-blue-500 text-white p-3 rounded-full hover:p-4 transition-all">Buy NFT</button></div>
     
     
@@ -77,25 +77,19 @@ const Marketplace = ()=>{
 
 
      <div className="size-fit" >
-  <div className="box-border bg-gray-200 border-2 box-content space-y-2 rounded-lg drop-shadow-xl shadow-black "> 
+  <div className="rounded-lg bg-black bg-opacity-15  space-y-2 shadow-sm shadow-black bg-opacity-25 mb-8 "> 
       <img className="size-72" src="https://www.patriot-place.com/wp-content/uploads/2023/01/Ed-Sheeran-2023-square-2.jpg"></img>  
-      <div className=""><h1 className="font-bold text-justify  size-auto">Ed Sheeran Mathematics Tour </h1>
-     <div className="pb-3 mt-2 flex  space-x-3 place-items-center  "><p className="text-black font-bold font-mono">Price-$130.00</p>
+      <div className=""><h1 className="font-bold text-white  size-auto">Ed Sheeran Mathematics Tour </h1>
+     <div className="pb-3 mt-2 flex  space-x-3 place-items-center  "><p className="text-white font-bold font-mono">Price-$130.00</p>
      <button className=" font-semibold   bg-gradient-to-r from-cyan-500 to-blue-500 text-white p-3 rounded-full hover:p-4 transition-all">Buy NFT</button></div>
     
       </div> 
   </div>
 </div>
 </main>
-<footer className="flex items-center pt- m-auto ml-14 mr-14 border-t-2  border-black">
-<img src="https://is4-ssl.mzstatic.com/image/thumb/Purple62/v4/08/e9/de/08e9de3e-cbed-4a0f-ac75-a18c38bec95c/source/512x512bb.jpg" className="size-28"></img>
-<div className="ml-auto mr-0 flex space-x-4">
-   <a href=""> <img className="size-12" src="https://media.discordapp.net/attachments/544823021089849354/1199712539546755092/gmail.png?ex=65c38a5f&is=65b1155f&hm=b898033701d4df654a9c1791f3ec3db088a43f94e2aa72663c1e36e08d9159f8&=&format=webp&quality=lossless&width=694&height=694"></img></a>
-   <a href=""> <img className="size-12 " src="https://cdn.discordapp.com/attachments/544823021089849354/1199714280703013004/twitter.png?ex=65c38bfe&is=65b116fe&hm=d7a87b67cf5040882865159ec43d0784f0dc5e1858af0ed36854047702c164cd&"></img></a>
-</div>
-</footer>
 
-</body>
+
+</main>
     )
 }
  
